755bb3c19ab915550b7ef461b0793a4fa1b66c1b
wxyz-claw <wxyz.claw@gmail.com>
2026-07-05 04:14:20 -0400

    Bump eye rest PWA cache for minimalist redesign

--- eye-rest/sw.js
+++ eye-rest/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eye-rest-timer-v4";
+const CACHE_NAME = "eye-rest-timer-v5";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
