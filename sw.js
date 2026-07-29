cc28456b35b3fd1423b43f272d2ccad74edcd56e
wxyz-jason <wxyz-jason@gmail.com>
2026-07-29 00:44:22 -0400

    Reading Stack: add “The harness is all you need (mostly)” (saved copy + summary)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *  - Google Fonts: stale-while-revalidate.
  * Bump VERSION when shell assets change.
  */
-const VERSION = 'v8';
+const VERSION = 'v9';
 const SHELL_CACHE = `clawd-shell-${VERSION}`;
 const RUNTIME_CACHE = `clawd-runtime-${VERSION}`;
 const FONT_CACHE = `clawd-fonts-${VERSION}`;
